81fdb95b3e0981294ab496d4305eb35b4d0293d4
Savva <savva08savva08@gmail.com>
2024-02-23 00:06:15 +0300

    fix API

--- callbacks/menu_callbacks.py
+++ callbacks/menu_callbacks.py
@@ -6,11 +6,12 @@ from keyboards.delete_keyboard import get_delete_keyboard
 import requests
 from keyboards.payment_keyboard import get_pay_keyboard
 import httpx
-
+import json
 
 
 router = Router()
-
+config = json.loads(open("config.json").read())
+ROCKET = config['XROCKET']
 
 @router.callback_query(F.data.startswith("menu"))
 async def main_menu(callback: types.CallbackQuery):
@@ -35,7 +36,7 @@ async def main_menu(callback: types.CallbackQuery):
                 "expiredIn": 9999
                 }
             async with httpx.AsyncClient() as client:
-                r = await client.post("https://pay.ton-rocket.com/tg-invoices", json=req, headers={"Rocket-Pay-Key": "891cdb097a088bf6f9d19de24"})
+                r = await client.post("https://pay.ton-rocket.com/tg-invoices", json=req, headers={"Rocket-Pay-Key": "{ROCKET}"})
             
             link = r.json()['data']['link']
             await callback.message.edit_reply_markup(reply_markup=get_pay_keyboard(link))
